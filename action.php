5e91c991df21b6932cf9431de4103c0cef8bcf77
Ваше Имя <you@example.com>
2018-11-14 14:02:28 +1000

    moding and function

--- action.php
+++ action.php
@@ -1,34 +1,46 @@
 <?php
+function calc(){
 if (isset($_POST['btn'])) {
- 	
-
 
 	$data=$_POST;
-	$pic1=$data['number1'];
-	$pic2=$data['number2'];
+	if(is_int($data['number1']) and is_int($data['number2'] )){
+
+		$pic1=$data['number1'];
+		$pic2=$data['number2'];
+	}
+
+	else{
+		$answer='error x';
+		return $answer;
+		//break;
+	}
+
 	$act=$data['act'];
-		
+	$answer = 0;
 
 	switch ($act) {
-		case '/':
-			echo "$pic1/$pic2=";
-			echo $pic1/$pic2;
+					case '/':
+			$answer = "$pic1 / $pic2 = ".$pic1/$pic2;
 			break;
-		case '*':
-			echo "$pic1*$pic2=";
-			echo $pic1*$pic2;
+					case '*':
+			$answer =  "$pic1 * $pic2 = ".$pic1*$pic2;
 			break;
 					case '-':
-			echo "$pic1-$pic2=";
-			echo $pic1-$pic2;
+			$answer =  "$pic1 - $pic2 = ".$pic1-$pic2;
 			break;
 					case '+':
-			echo "$pic1+$pic2=";
-			echo $pic1+$pic2;
+			$answer =  "$pic1 + $pic2 = ".$pic1+$pic2;
 			break;		
-		default:
-			echo "WTF????";
+					case 'Sqr':
+			$answer =  "<sup>$pic2</sup> √ $pic1 = ".pow($pic1, 1/$pic2); 
+			break;		
+					case 'Pwr':
+			$answer =  "$pic1 <sup>$pic2</sup> = ".pow($pic1, $pic2);
+			break;		
+					default:
+			$answer =  "WTF?";
 			break;
 	}
-	echo '<br><a href="index.html">back</a>';
+	return $answer;
+	}
 }
--- index.php
+++ index.php
@@ -1,21 +1,30 @@
+<?php
+include 'action.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" type="text/css" href="index.css">
+<link rel="stylesheet" type="text/css" href="Style.css">
 <meta charset="UTF-8">
 <head>
 <body>
-	<form method="POST" action="action.php">
+	<form method="POST" action="">
 		<input class="a1" type="text" value="" name="number1"></input>
+			<select name="act" >
+				<option value="/">/</option>
+				<option value="*">*</option>
+				<option value="-">-</option>
+				<option value="+">+</option>
+				<option value="Sqr">Sqr</option>
+				<option value="Pwr">Pwr</option>
+			</select>
 		<br><input class="a1" type="text" value="" name="number2"></input>
-		<select name="act">
-			<option value="/">/</option>
-			<option value="*">*</option>
-			<option value="-">-</option>
-			<option value="+">+</option>
-		</select>
 		<input type="submit" value="calc" name="btn"></input>
-
 	</form>
+<h1>
+	<?php
+	echo calc();
+	?>
+</h1>
 </body>
 </head>
 	<!-- <form action="index.php" method="POST">
